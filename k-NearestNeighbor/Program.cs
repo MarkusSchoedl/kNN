cfc98f83044a7f242cf17d9c4c4ce6915cd26aa9
Markus Schoedl <if16b034@technikum-wien.at>
2018-03-08 17:36:58 +0100

    Finished splitting the data

--- k-NearestNeighbor/Program.cs
+++ k-NearestNeighbor/Program.cs
@@ -6,9 +6,16 @@ namespace k_NearestNeighbor
     {
         static void Main(string[] args)
         {
-            WhiteWine_kNN kNN = new WhiteWine_kNN(@"C:\Users\marku\Dropbox\FH\MLE\white_wine\winequality-white.csv");
-            kNN.Start_kNN();
-
+            WhiteWine_kNN kNN = new WhiteWine_kNN();
+            if (kNN.ReadWineData(@"C:\Users\marku\Dropbox\FH\MLE\white_wine\winequality-white.csv"))
+            {
+                //kNN.EnableLogging();
+                kNN.Start_kNN(13);
+            }
+            else
+            {
+                Console.WriteLine("Error parsing the file.");
+            }
             Console.ReadKey();
         }
     }
--- k-NearestNeighbor/WhiteWine.cs
+++ k-NearestNeighbor/WhiteWine.cs
@@ -9,6 +9,8 @@ namespace k_NearestNeighbor
     class WhiteWine_kNN
     {
         #region Parameters
+        private bool _LoggingEnabled;
+
         // Key: Each Wine Attributes, Value: Wine Quality
         private Dictionary<int, List<WineAttributes>> _TotalWineData = new Dictionary<int, List<WineAttributes>>();
         private Dictionary<int, List<WineAttributes>> _DoubleWineData = new Dictionary<int, List<WineAttributes>>();
@@ -21,50 +23,74 @@ namespace k_NearestNeighbor
         /// <summary>
         /// Creates a new Instance for a kNN-Algorithm for WhiteWine.
         /// </summary>
-        public WhiteWine_kNN(string filename)
+        public WhiteWine_kNN()
         {
             for (int i = 0; i <= 10; i++)
             {
                 _TotalWineData.Add(i, new List<WineAttributes>());
                 _DoubleWineData.Add(i, new List<WineAttributes>());
             }
-
-            if (ReadFile(filename))
-            {
-                Console.WriteLine("Found " + _TotalWineData.Values.Sum(list => list.Count) + " unique wine datasets.");
-                Console.WriteLine("Found " + _DoubleWineData.Values.Sum(list => list.Count) + " redundant wine datasets.\n");
-
-                foreach (var dataSet in _TotalWineData.Where(x => x.Value.Count() > 0))
-                {
-                    Console.WriteLine("quality: " + dataSet.Key + " - wines: " + dataSet.Value.Count());
-                }
-            }
-            else
-            {
-                Console.WriteLine("Error while reading Data.");
-                throw new InvalidDataException();
-            }
         }
         #endregion
 
         #region Methods
+        public void EnableLogging()
+        {
+            _LoggingEnabled = true;
+        }
+
         public bool Start_kNN(int k)
         {
             #region Calculate Chunk Size
-            for(int i = 0; i < k; i++)
+            for (int chunk = 0; chunk < k; chunk++)
             {
-                _Chunks.Add(i, new )
+                _Chunks.Add(chunk, new List<WineAttributes>());
 
-                foreach (var lst in _TotalWineData.Where(x => x.Value.Count() > 0))
+                foreach (var indList in _TotalWineData.Where(x => x.Value.Any()))
                 {
-                    if (lst.Value.Count() % k == 0)
-                    {
+                    var lst = indList.Value;
+                    int quality = indList.Key;
 
-                    }
-                    else
-                    {
+                    int offset = (lst.Count * ((chunk + 1) / (float)k)) % 1 == 0 ? 0 : 1;
 
+                    if(_LoggingEnabled) Console.WriteLine("i = " + (int)(lst.Count * (chunk / (float)k)) + "; i < " + ((lst.Count * ((chunk + 1) / (float)k)) - offset));
+
+                    for (int i = (int)(lst.Count * (chunk / (float)k)); i < (lst.Count * ((chunk + 1) / (float)k)) - offset; i++)
+                    {
+                        _Chunks[chunk].Add(_TotalWineData[quality][i]);
                     }
+                }
+
+                if (_LoggingEnabled) Console.WriteLine("\n");
+            }
+
+            if (_LoggingEnabled)
+            {
+                Console.WriteLine("Count After splitting data: " + _Chunks.Values.Sum(list => list.Count));
+
+                foreach (var dataSet in _Chunks)
+                {
+                    Console.WriteLine("ChunkNum: " + dataSet.Key + " - ChunkSize: " + dataSet.Value.Count());
+                }
+
+                Dictionary<int, List<WineAttributes>> chunkData = new Dictionary<int, List<WineAttributes>>();
+                _TotalWineData.ToList().ForEach(x => chunkData.Add(x.Key, new List<WineAttributes>()));
+                foreach (var c in _Chunks)
+                {
+                    chunkData.ToList().ForEach(x => x.Value.Clear());
+
+                    foreach (var x in c.Value)
+                    {
+                        int quality = 0;
+                        _TotalWineData.ToList().ForEach(z => { if (z.Value.Contains(x)) { quality = z.Key; } });
+                        chunkData[quality].Add(x);
+                    }
+
+                    foreach (var dataSet in chunkData)
+                    {
+                        Console.WriteLine("quality: " + dataSet.Key + " - wines: " + dataSet.Value.Count());
+                    }
+                    Console.WriteLine("\n");
                 }
             }
             #endregion
@@ -76,7 +102,7 @@ namespace k_NearestNeighbor
         #endregion
 
         #region File Parsing 
-        private bool ReadFile(string filename)
+        public bool ReadWineData(string filename)
         {
             try
             {
@@ -105,6 +131,17 @@ namespace k_NearestNeighbor
             catch (FileNotFoundException)
             {
                 return false;
+            }
+
+            if (_LoggingEnabled)
+            {
+                Console.WriteLine("Found " + _TotalWineData.Values.Sum(list => list.Count) + " unique wine datasets.");
+                Console.WriteLine("Found " + _DoubleWineData.Values.Sum(list => list.Count) + " redundant wine datasets.\n");
+
+                foreach (var dataSet in _TotalWineData.Where(x => x.Value.Count() > 0))
+                {
+                    Console.WriteLine("quality: " + dataSet.Key + " - wines: " + dataSet.Value.Count());
+                }
             }
 
             return true;
